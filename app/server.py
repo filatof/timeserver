2a3eaad4d63bdad2845cb6918c0317b367dd658d
Ivan A. Filatoff <fill@MacBookAir.local>
2025-08-01 21:25:57 +0300

    fix ingress hpa server

--- app/server.py
+++ app/server.py
@@ -18,11 +18,12 @@ class RequestHandler(BaseHTTPRequestHandler):
                     self.respond_with(200, "Ready")
                 else:
                     self.respond_with(503, "Not Ready")
-            case _:
-                self.respond_with(404, "Not Found")
             case '/pi':
                 pi = leibniz_pi(1000000)
                 self.respond_with(200, str(pi))
+            case _:
+                self.respond_with(404, "Not Found")
+        
 
     def respond_with(self, status_code: int, content: str) -> None:
         self.send_response(status_code)
